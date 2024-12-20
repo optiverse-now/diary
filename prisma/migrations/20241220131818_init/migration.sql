60fa3fd94a4d36b5ff52b07cd05b60b2bf8e0db1
optiverse.now <optiverse.now@gmail.com>
2024-12-20 23:43:18 +0900

    prismaによるdbの構築とマイグレーション

---
+++ prisma/migrations/20241220131818_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Diary" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "mood" TEXT NOT NULL,
+    "tags" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Diary_pkey" PRIMARY KEY ("id")
+);
