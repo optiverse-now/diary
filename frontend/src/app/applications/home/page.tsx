cda0c8a92f8248e5cb0ad85e7dc012d2dee22fb2
optiverse.now <optiverse.now@gmail.com>
2024-12-21 23:18:15 +0900

    開発中の明記

--- frontend/src/app/applications/home/page.tsx
+++ frontend/src/app/applications/home/page.tsx
@@ -1,12 +1,14 @@
-import { AppSidebar } from "../../../components/organisms/AppSidebar"
+'use client'
+
+import { MainLayout } from "../../../components/templates/MainLayout"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen">
-      <AppSidebar />
-      <main className="flex-1">
-        <h1>ホーム</h1>
-      </main>
-    </div>
-  );
+    <MainLayout>
+      <div className="flex flex-col items-center justify-center min-h-[calc(100vh-2rem)] p-6">
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">Coming Soon</h1>
+        <p className="text-lg text-gray-600">新機能を開発中です。もうしばらくお待ちください。</p>
+      </div>
+    </MainLayout>
+  )
 }
